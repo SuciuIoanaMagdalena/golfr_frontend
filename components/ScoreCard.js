8dca421c8c8b6c511913fe090b7f96650fc6e3f5
SuciuIoanaMagdalena <ioana.suciu@golfgenius.com>
2022-02-04 15:36:02 +0200

    Golfers page

--- components/ScoreCard.js
+++ components/ScoreCard.js
@@ -1,12 +1,12 @@
+import Link from 'next/link'
 import PropTypes from 'prop-types'
 import useScoreDelete from '../lib/useScoreDelete'
 import { getUserId } from '../lib/userAuth'
-
+import { useRouter } from 'next/router'
 const CONFIRM_MESSAGE = 'Are you sure you want to delete the score?'
 
 const ScoreCard = ({ id, playedAt, totalScore, userId, userName }) => {
   const { deleteScore } = useScoreDelete(id)
-
   return (
     <div className="flex flex-row p-3 my-4 shadow-md lg:w-1/3 md:w-1/2">
       <div className="w-5/6">
@@ -14,7 +14,10 @@ const ScoreCard = ({ id, playedAt, totalScore, userId, userName }) => {
           {playedAt}
         </div>
         <div>
-          {`${userName} posted a score of ${totalScore}`}
+          <Link href={`golfers/${userId}`}>
+            <a className="underline text-blue-700">{`${userName}`}</a>
+          </Link>
+          {` posted a score of ${totalScore}`}
         </div>
       </div>
       <div className="w-1/6">
---
+++ pages/golfers/[id].js
@@ -0,0 +1,67 @@
+import Layout from '../../components/Layout'
+import ScorePostWidget from '../../components/ScorePostWidget'
+import ScoreCard from '../../components/ScoreCard'
+import { useEffect, useState } from 'react'
+import { getToken, getUsername } from '../../lib/userAuth'
+import { useRouter } from 'next/router'
+import useUserScores from '../../lib/useUserScores'
+import { USER_SCORES_URL } from '../../lib/useUserScores'
+import { useUserName } from '../../lib/useUserName'
+import { USER_URL } from '../../lib/useUserName'
+
+
+
+const Profile = () => {
+  const router = useRouter()
+  const { id } = router.query
+  // const [ scores, setScores ] = useState([])
+  // const [ scores, error ] = useState('')
+  const [ name, setName ] = useState('')
+  const [ message, setMessage ] = useState('')
+  const { scores, error } = useUserScores(id)
+  console.log(scores);
+  useEffect(() => {
+    // console.log(scores[0].user_name)
+    if (scores.length > 0){
+      setName(scores[0].user_name)
+    }
+
+  }, [ scores ] )
+  useEffect(() => {
+    if (scores.length < 0) {
+      setMessage('has no scores')
+    } else {
+      setMessage('has the following scores')
+    }
+  }, [ scores ])
+
+  // console.log('Message' + message)
+
+
+
+
+  return (
+    <Layout>
+      <>
+        {error ? (
+          error
+        ) : (
+          <>
+            <h1> {name} {message}  </h1>
+            {scores && scores.map(score => (
+              <ScoreCard
+                key={score.id}
+                id={score.id}
+                totalScore={score.total_score}
+                playedAt={score.played_at}
+                userId={score.user_id}
+                userName={score.user_name}
+              />
+            ))}
+          </>
+        )}
+      </>
+    </Layout>
+  )
+}
+export default Profile
